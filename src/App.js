e8c8ad8b0dc840814ede92741fc4ca908ddd3e27
Aleksandr Abramov <yamyaso@gmail.com>
2022-10-24 22:38:53 +0300

    fix link

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://steamuserimages-a.akamaihd.net/ugc/1687121178871562855/992BF7ADFC86292A1AFDEA072A1AF7FEF45D3999/?imw=5000&imh=5000&ima=fit&impolicy=Letterbox&imcolor=%23000000&letterbox=false"
           target="_blank"
           rel="noopener noreferrer"
         >
